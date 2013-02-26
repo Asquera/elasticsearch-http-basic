f7f031df56ebb94590bcf9820455afd527eeae8d
Peter <peathal@yahoo.de>
2013-02-26 14:53:26 +0100

    enabled by default

--- src/main/java/com/asquera/elasticsearch/plugins/http/HttpBasicServerPlugin.java
+++ src/main/java/com/asquera/elasticsearch/plugins/http/HttpBasicServerPlugin.java
@@ -10,13 +10,17 @@ import org.elasticsearch.common.settings.ImmutableSettings;
 import java.util.Collection;
 
 import static org.elasticsearch.common.collect.Lists.*;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
 
 /**
  * @author Florian Gilcher (florian.gilcher@asquera.de)
  */
 public class HttpBasicServerPlugin extends AbstractPlugin {
+
+    private boolean enabledByDefault = true;
     private final Settings settings;
-    
+
     @Inject public HttpBasicServerPlugin(Settings settings) {
         this.settings = settings;
     }
@@ -28,26 +32,31 @@ public class HttpBasicServerPlugin extends AbstractPlugin {
     @Override public String description() {
         return "HTTP Basic Server Plugin";
     }
-    
+
     @Override public Collection<Class<? extends Module>> modules() {
         Collection<Class<? extends Module>> modules = newArrayList();
-        if (settings.getAsBoolean("http.basic.enabled", false)) {
+        if (settings.getAsBoolean("http.basic.enabled", enabledByDefault)) {
             modules.add(HttpBasicServerModule.class);
         }
         return modules;
     }
-    
+
     @Override public Collection<Class<? extends LifecycleComponent>> services() {
         Collection<Class<? extends LifecycleComponent>> services = newArrayList();
-        if (settings.getAsBoolean("http.basic.enabled", false)) {
+        if (settings.getAsBoolean("http.basic.enabled", enabledByDefault)) {
             services.add(HttpBasicServer.class);
         }
         return services;
     }
-    
+
     @Override public Settings additionalSettings() {
-        if (settings.getAsBoolean("http.basic.enabled", false)) {
-            return ImmutableSettings.settingsBuilder().put("http.enabled", false).build();
+        Loggers.getLogger(getClass()).info("now {}", settings);
+        if (settings.getAsBoolean("http.basic.enabled", enabledByDefault)) {
+            return ImmutableSettings.settingsBuilder().
+                    put("http.basic.user", "admin").
+                    put("http.basic.password", "admin_pw").
+                    put("http.enabled", false).                    
+                    build();
         } else {
             return ImmutableSettings.Builder.EMPTY_SETTINGS;
         }
