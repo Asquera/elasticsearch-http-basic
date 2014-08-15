df049cc2289b92b191ade83a47c7d25981f70b26
Hugues Malphettes <hmalphettes@gmail.com>
2014-08-15 19:34:29 +0800

    Restore support for http.basic.enabled: false in the settings

--- src/main/java/com/asquera/elasticsearch/plugins/http/HttpAuthenticationPlugin.java
+++ src/main/java/com/asquera/elasticsearch/plugins/http/HttpAuthenticationPlugin.java
@@ -1,11 +1,21 @@
 package com.asquera.elasticsearch.plugins.http;
 
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.rest.RestModule;
 
 /**
  */
 public class HttpAuthenticationPlugin  extends AbstractPlugin {
+
+	 private boolean enabledByDefault = true;
+	 private final Settings settings;
+
+	 @Inject public HttpAuthenticationPlugin(Settings settings) {
+		 this.settings = settings;
+	 }
+
 
     @Override public String name() {
         return "http-auth-plugin";
@@ -16,7 +26,9 @@ public class HttpAuthenticationPlugin  extends AbstractPlugin {
     }
 
     public void onModule(RestModule restModule) {
+      if (settings.getAsBoolean("http.basic.enabled", enabledByDefault)) {
         restModule.addRestAction(HttpBasicAuthRestAction.class);
+      }
     }
 
 }
