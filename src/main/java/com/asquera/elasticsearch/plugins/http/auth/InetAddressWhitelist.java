b84035c4abcd133d3ef3812bb4ebb699aef01d72
mattk42 <matt1988@gmail.com>
2015-07-08 10:27:14 -0600

    Move to regex Patterns instead of InetAddresses in the whitelist

--- src/main/java/com/asquera/elasticsearch/plugins/http/auth/InetAddressWhitelist.java
+++ src/main/java/com/asquera/elasticsearch/plugins/http/auth/InetAddressWhitelist.java
@@ -7,28 +7,30 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Iterator;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
  *
  * Wraps the configured whitelisted ips.
- * It uses a set of {@link InetAddress} internally.
+ * It uses a set of {@link Pattern} internally.
  * <p>
  *
  *
  *
- * @author Ernesto Miguez (ernesto.miguez@asquera.de)
+ * @author Matthew Knox
  */
 
 public class InetAddressWhitelist {
-  private Set<InetAddress> whitelist;
+  private Set<Pattern> whitelist;
+  
   /**
    *
    *
    * @param whitelist
    */
-  public InetAddressWhitelist(Set<InetAddress> whitelist) {
+  public InetAddressWhitelist(Set<Pattern> whitelist) {
     this.whitelist = whitelist;
   }
 
@@ -39,44 +41,49 @@ public class InetAddressWhitelist {
    *
    */
   public InetAddressWhitelist(String[] sWhitelist) {
-    this(toInetAddress(Arrays.asList(sWhitelist)));
+    this(toPattern(Arrays.asList(sWhitelist)));
   }
 
   /**
    * Checks the request ip for inclusion.
-   * Since that ip comes in a {@link InetAddress} representation, it is checked
-   * against the whitelist.
+   * Since that ip comes in a {@link InetAddress} representation, the IP 
+   * string is grabbed and compared to the patterns.
    *
    * @param candidate
    * @return if the ip is included in the whitelist
    */
   public Boolean contains(InetAddress candidate) {
-    return this.whitelist.contains(candidate);
+	return contains(candidate.getHostAddress());
   }
 
   /**
    *
-   * Checks the xForwardedFor defined client ip for inclusion.
-   * Since that ip comes in a String representation, it is checked against
-   * the String representation of the defined whitelist.
+   * Checks a defined client ip for inclusion.
+   * Since that ip comes in a String representation, it is matched against
+   * the patterns defined in the whitelist.
    *
    * @param candidate
    * @return if the ip is included in the String representation of the
    * whitelist ips
    */
   public Boolean contains(String candidate) {
-    return getStringWhitelist().contains(candidate);
+	   	for (Pattern pattern : whitelist){
+	   		if (pattern.matcher(candidate).matches()){
+	   			return true;
+	   		}
+	   	}  
+	   	return false;
   }
 
   /**
    * @return set of the string representations of the whitelist
    */
   Set<String> getStringWhitelist() {
-    Iterator<InetAddress> iterator = this.whitelist.iterator();
+    Iterator<Pattern> iterator = this.whitelist.iterator();
     Set<String> set = new HashSet<String>();
     while (iterator.hasNext()) {
-      InetAddress next = iterator.next();
-      set.add(next.getHostAddress());
+      Pattern next = iterator.next();
+      set.add(next.toString());
     }
     return set;
   }
@@ -89,20 +96,20 @@ public class InetAddressWhitelist {
    * @return a list of {@link InetAddress} objects
    *
    */
-  static Set<InetAddress> toInetAddress(List<String> ips) {
-    List<InetAddress> listIps = new ArrayList<InetAddress>();
+  static Set<Pattern> toPattern(List<String> ips) {
+    List<Pattern> listIps = new ArrayList<Pattern>();
     Iterator<String> iterator = ips.iterator();
     while (iterator.hasNext()) {
       String next = iterator.next();
       try {
-        listIps.add(InetAddress.getByName(next));
-      } catch (UnknownHostException e) {
-        String template = "an ip set in the whitelist settings raised an " +
-          "UnknownHostException: {}, dropping it";
+        listIps.add(Pattern.compile(next));
+      } catch (Exception e) {
+        String template = "an ip set in the whitelist settings raised an Exception, dropping it:" +
+           e.getMessage();
         Loggers.getLogger(InetAddressWhitelist.class).info(template, e.getMessage());
       }
     }
-    return new HashSet<InetAddress>(listIps);
+    return new HashSet<Pattern>(listIps);
   }
 
   /**
